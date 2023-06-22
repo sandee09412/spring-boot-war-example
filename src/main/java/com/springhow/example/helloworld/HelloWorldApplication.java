2c9c2475ee9e6166b1419fd4703b35125b3dde44
sandee09412 <32383286+sandee09412@users.noreply.github.com>
2023-06-22 15:44:45 +0530

    Update HelloWorldApplication.java

--- src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
@@ -23,7 +23,7 @@ public class HelloWorldApplication extends SpringBootServletInitializer {
 
     @RequestMapping("/")
     String helloWorld() {
-        return "Welcome to Sandeep CI/CD implementation with Jenkins tools";
+        return "Welcome to CI/CD implementation with Jenkins tools";
     }
 
 }
